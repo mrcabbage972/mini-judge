065cb87c74a4571633d96c458ad88f201ca6568d
mrcabbage972 <mayvic@gmail.com>
2023-09-27 18:06:13 +0000

    fix: adding long desc to package

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 setup(
     name='mini_judge',
     description='Simple implementation of LLM-As-Judge',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Victor May',
     version='0.1.0',
     packages=find_packages(where='src'),
