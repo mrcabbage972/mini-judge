2b6c0050e4430e9ca96619de293d4ba1feaedd5e
mrcabbage972 <mayvic@gmail.com>
2023-10-11 20:32:42 +0000

    feat: changing judge prompt to be same as in arena repo

--- test/test_judge.py
+++ test/test_judge.py
@@ -1,2 +1,12 @@
-def test_judge():
-    pass
+from mini_judge.judge_prompt import judge_prompt_template
+
+
+def test_judge_prompty_format():
+    question = "aaa"
+    candidate_answer = "bbb"
+    ref_answer = "ccc"
+    prompt = judge_prompt_template.format(question=question, candidate_answer=candidate_answer,
+                                          ref_answer=ref_answer)
+    assert question in prompt
+    assert candidate_answer in prompt
+    assert ref_answer in prompt
